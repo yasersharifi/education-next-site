e7e446a80ad1fefdeeadbe388cf876a1f3f06c16
Yaser Sharifi <yassersharifi74@gmail.com>
2023-05-25 18:36:38 +0330

    setting tailwind and material tailwind and font on project

---
+++ src/mt-react/index.ts
@@ -0,0 +1,19 @@
+"use client";
+
+import {
+    Navbar,
+    Collapse,
+    Typography,
+    Button,
+    IconButton,
+    Card,
+} from "@material-tailwind/react";
+
+export {
+    Navbar,
+    Collapse,
+    Typography,
+    Button,
+    IconButton,
+    Card, 
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = {
+module.exports = withMT({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,4 +16,4 @@ module.exports = {
       },
     }
   },
-}
+})
